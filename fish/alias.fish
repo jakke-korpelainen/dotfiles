684b7fa88dc66ef2aa7e8477d25fab58f0522686
Jakke Korpelainen <3626319+jakke-korpelainen@users.noreply.github.com>
2025-12-07 17:11:43 +0200

    initial commit

---
+++ fish/alias.fish
@@ -0,0 +1,37 @@
+alias dev='cd ~/Development'
+alias zed='zeditor'
+alias gs='git status'
+alias add='git add .'
+alias gp='git pull'
+
+alias latest='git tag --sort=-v:refname | head -n 1 | sed 's/^v//''
+alias git-branches='git branch --sort=-committerdate'
+alias main='git checkout main'
+
+function gnb --argument-names branch
+    git checkout -b $branch
+end
+
+function gb --argument-names branch
+    git checkout $branch
+end
+
+function commit --argument-names arg1
+    git commit -m"$arg1"
+end
+
+function chore --argument-names msg
+    commit "chore: $msg"
+end
+
+function feat --argument-names msg
+    commit "feat: $msg"
+end
+
+function fix --argument-names msg
+    commit "fix: $msg"
+end
+
+function refactor --argument-names msg
+    commit "refactor: $msg"
+end
---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+source /home/jwekke/.config/fish/alias.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+function fish_greeting
+#    # smth smth
+end
