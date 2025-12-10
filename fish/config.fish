bae1b83eb3cb614d37d2f11cfed297a7aa95fd7b
Jakke Korpelainen <3626319+jakke-korpelainen@users.noreply.github.com>
2025-12-10 21:50:20 +0200

    chore: source .profile in fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 source /home/jwekke/.config/fish/alias.fish
+source /home/jwekke/.profile
 
 # overwrite greeting
 # potentially disabling fastfetch
