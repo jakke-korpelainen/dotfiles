7ae0a6ff24986ebd2639dfc26b571c092c9fec1b
Jakke Korpelainen <3626319+jakke-korpelainen@users.noreply.github.com>
2025-12-10 21:43:17 +0200

    chore: remove obsolete mistral ai function

--- fish/alias.fish
+++ fish/alias.fish
@@ -1,5 +1,7 @@
 # aliases
 alias docker='echo "# re-routing to podman " && podman'
+alias explorer='yazi'
+alias files='yazi'
 alias calc='kalker'
 alias dev='cd ~/Development'
 alias zed='zeditor'
@@ -38,39 +40,3 @@ end
 function refactor --argument-names msg
     commit "refactor: $msg"
 end
-
-function mistral_chat --argument-names message
-    set -l json_data (jq -n --arg message "$message" '{
-        model: "mistral-large-latest",
-        messages: [
-            {
-                role: "user",
-                content: $message
-            }
-        ],
-        stream: true
-    }')
-
-    set -l full_stream ""
-
-    curl \
-        --location "https://api.mistral.ai/v1/chat/completions" \
-        --header 'Content-Type: application/json' \
-        --header 'Accept: application/json' \
-        --header "Authorization: Bearer $MISTRAL_API_KEY" \
-        --data "$json_data" \
-        --silent \
-        --no-buffer \
-            | while read -l line
-                if string match -q -- "data: [DONE]" $line
-                    break
-                end
-                set -l json_line (echo $line | sed 's/^data: //')
-                set -l content (echo $json_line | jq -r '.choices[0].delta.content // empty' | sed 's/\r/\n/g')
-                set full_stream "$full_stream$content"
-                echo -n $content
-            end
-    echo -n "$full_stream" | glow -
-end
-
-alias ai='mistral_chat'
